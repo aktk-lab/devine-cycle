d97890b2c3a4ef7eaca6b369e93d7eb4cd4081fe
aktk-lab <worldflower333@gmail.com>
2025-08-15 16:32:50 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,33 @@
+const CACHE_NAME = "divine-cycle-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(net => {
+        if (e.request.method === "GET" &&
+            new URL(e.request.url).origin === location.origin) {
+          const copy = net.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+        }
+        return net;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
